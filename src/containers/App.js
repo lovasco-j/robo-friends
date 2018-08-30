072d87db6622a8149180370f07c0faa9d3fbc480
Joseph LoVasco <joseph.lovasco@globe.com>
2018-08-30 11:35:40 -0400

    Restructuring the <App> for easier testing

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,6 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import Counter from '../components/Counter';
-import CardList from '../components/CardList';
-import Scroll from '../components/Scroll';
-import SearchBox from '../containers/SearchBox';
-import ErrorBoundary from '../containers/ErrorBoundary';
-import './App.css'
+import React from 'react';
+import { connect } from 'react-redux';
+import Main from '../components/Main';
 
 import {
     setSearchField,
@@ -30,46 +25,6 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-class App extends Component {
-    componentDidMount() {
-        this.props.onRequestRobots();
-    }
-
-    render() {
-        const {
-            searchField,
-            onSearchChange,
-            robots,
-            isPending,
-            error
-        } = this.props;
-
-        let filterRobots = robots.filter((robot) => {
-            return robot.name.toLowerCase().includes(searchField.toLowerCase())
-        });
-
-
-        if (isPending) {
-            return (<div className="tc">
-                <h1>RoboFriends</h1>
-                <SearchBox onSearchChange={onSearchChange}/>
-                <p>loading...</p>
-            </div>);
-        } else {
-            return (
-                <div className="tc">
-                    <h1>RoboFriends</h1>
-                    <Counter/>
-                    <SearchBox onSearchChange={onSearchChange}/>
-                    <Scroll>
-                        <ErrorBoundary>
-                            <CardList robots={filterRobots}/>
-                        </ErrorBoundary>
-                    </Scroll>
-                </div>
-            );
-        }
-    }
-}
+const App = (props) => <Main {...props} />;
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
