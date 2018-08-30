a807236bb2b026eecb512f4f9118a2ff8d1c5e5a
Joseph LoVasco <joseph.lovasco@globe.com>
2018-08-30 16:24:04 -0400

    Adding a few more tests to the searchbox

--- src/containers/SearchBox.test.js
+++ src/containers/SearchBox.test.js
@@ -10,7 +10,12 @@ it('<SearchBox> should call the props.onSearchChange on change', () => {
     const mockOnSearchChange = jest.fn();
 
     const wrapper = shallow(<SearchBox onSearchChange={mockOnSearchChange} />);
-    wrapper.find('input').simulate('change');
+    wrapper.find('input').simulate('change', {event: { target: { value: 'h'} } });
 
     expect(mockOnSearchChange).toBeCalled();
+    expect(mockOnSearchChange.mock.calls.length).toEqual(1);
+    // calls is an array of arguments arrays for every time the mock() was called.
+    expect(mockOnSearchChange.mock.calls[0][0])
+        .toEqual({event: { target: { value: 'h'} } });
+
 });
